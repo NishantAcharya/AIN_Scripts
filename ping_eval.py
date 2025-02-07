8796dc1719ca22172f4cc3f6d2ea3f3b5bee70c7
Nish Acharya <nacharya@ucdavis.edu>
2025-02-07 10:38:19 -0800

    ping_eval_initial_commit

--- ping_eval.py
+++ ping_eval.py
@@ -9,6 +9,19 @@ import random
 
 
 #Load all probes from the probe file
-#
+probes = []
+data = []
+with open('JSON/grouped_probes.json') as f:
+    data = json.load(f)
+
+for key in data.keys():
+    group = data[key]
+    for dist in group.keys():
+        probes.append(data[key][dist])
+
+#Load all the ping data
+pings = []
+
+#How many IPs out of all of these are close to the lib loc according to this! -- GOAL
 
     
