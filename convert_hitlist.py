4c8b732319e50a4687c1d5175f4e56fc4e4cb28a
Nishant Acharya <nacharya@ucdavis.edu>
2025-02-11 10:42:50 -0800

    Converted the hitlist data for easy processing

--- convert_hitlist.py
+++ convert_hitlist.py
@@ -1,5 +1,6 @@
 import json
 import csv
+from tqdm import tqdm
 
 def convert_hitlist(hitlist_file, output_file):
     with open(hitlist_file, 'r') as f:
@@ -7,15 +8,13 @@ def convert_hitlist(hitlist_file, output_file):
         data = {}
         for i in range(1,len(lines)-1):
             line = lines[i].split('\t')
-            score = line[0].strip()
-            ip = line[1].strip()
+            score = line[1].strip()
+            ip = line[-1].strip()
 
             data[ip] = score
     
     with open(output_file, 'w') as f:
         json.dump(data, f, indent=4)
-
-#def convert_support_file(support_file,output_file):
 
 def hex_to_ip(hex_string):
     if len(hex_string) != 8:
@@ -32,10 +31,29 @@ def hex_to_ip(hex_string):
 
     return ip_address
 
-# Example usage
-hex_string = "C0A80101"
-ip_address = hex_to_ip(hex_string)
-print(f"The IP address is: {ip_address}") # Output: The IP address is: 192.168.1.1
+def convert_hitlist_support(hitlist_file, output_file):
+    with open(hitlist_file, 'r') as f:
+        lines = f.readlines()
+        data = {}
+        for i in range(1,len(lines)-1):
+            line = lines[i].split('\t')
+            cidr = hex_to_ip(line[0].strip()) + '/24'
+            octets = line[1].strip().split(',')
+            if octets[0] == '-':
+                continue
+            chosen_ips = []
+            for octet in octets:
+                ip_last_octet = int(octet.strip(),16) #Hex String
+                ip = '.'.join(cidr.split('.')[:-1]) + '.' + str(ip_last_octet)
+                chosen_ips.append(ip)
+            data[cidr] = chosen_ips
 
+    
+    with open(output_file, 'w') as f:
+        json.dump(data, f, indent=4)
 
-#convert_hitlist('CSV/internet_address_hitlist_it109w-20240926.fsdb','JSON/hitlist.json')
+print('Converting Hitlist')
+convert_hitlist('CSV/internet_address_hitlist_it109w-20240926.fsdb','JSON/hitlist.json')
+print('Converting Hitlist Support')
+convert_hitlist_support('CSV/internet_address_verfploeter_hitlist_it109w-20240926.fsdb','JSON/hitlist_support.json')
+print('Done')
