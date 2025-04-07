a9f4931cc5a864fe15edd29776b266fb9d23c899
Anyusss <101853537+Anyusss@users.noreply.github.com>
2025-04-07 16:48:50 -0400

    Add files via upload

---
+++ delete_temporary_files.sh
@@ -0,0 +1,8 @@
+rm -rf ./FCC_Provider/FCC_providers.txt
+rm -rf ./reverse_geolocation/cidrs_near_library.txt
+rm -rf ./TF-IDF/provider_keywords.txt
+rm -rf ./TF-IDF/idf.txt
+rm -rf ./all_orgname.txt
+rm -rf ./final_cidrs.txt
+rm -rf ./providers_orgname.txt
+rm -rf ./outputs/*
