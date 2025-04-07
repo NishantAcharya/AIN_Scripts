c2cfc5c4c302cdb30b002c95d243b2052c368892
Nishant Acharya <nacharya@ucdavis.edu>
2025-04-06 23:52:02 -0700

    Added Base Files

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #Read the library names folder and create the arrays
 
+# Array of US state codes
+states=("AL" "AK" "AZ" "AR" "CA" "CO" "CT" "DE" "FL" "GA" "HI" "ID" "IL" "IN" "IA" "KS" "KY" "LA" "ME" "MD" "MA" "MI" "MN" "MS" "MO" "MT" "NE" "NV" "NH" "NJ" "NM" "NY" "NC" "ND" "OH" "OK" "OR" "PA" "RI" "SC" "SD" "TN" "TX" "UT" "VT" "VA" "WA" "WV" "WI" "WY")
+
+# Create folders for each state code
+for state in "${states[@]}"; do
+    if [ ! -d "$state" ]; then
+        mkdir "$state"
+    fi
+done
